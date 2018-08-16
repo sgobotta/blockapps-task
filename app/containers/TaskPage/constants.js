01a7f4d764bd1a226c81f04864c55450d02c170e
Santiago <sgobotta@gmail.com>
2018-08-16 15:39:18 -0300

    Defines new constants for the SW service

---
+++ app/containers/TaskPage/constants.js
@@ -0,0 +1,16 @@
+/*
+ * TaskConstants
+ * Each action has a corresponding type, which the reducer knows and picks up on.
+ * To avoid weird typos between the reducer and the actions, we save them as
+ * constants here. We prefix them with 'yourproject/YourComponent' so we avoid
+ * reducers accidentally picking up actions they shouldn't.
+ *
+ * Follow this format:
+ * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
+ */
+
+export const CHANGE_CATEGORY = 'boilerplate/Task/CHANGE_CATEGORY';
+
+export const LOAD_SWDATA = 'boilerplate/Task/LOAD_SWDATA';
+export const LOAD_SWDATA_SUCCESS = 'boilerplate/Task/LOAD_SWDATA_SUCCESS';
+export const LOAD_SWDATA_ERROR = 'boilerplate/Task/LOAD_SWDATA_ERROR';
